4e2ea3cf0d5e813df36a9a05d9649d674b27381b
Timyr486786866745 <90512751+Timyr486786866745@users.noreply.github.com>
2021-11-13 19:02:48 +0200

    Add files via upload

--- games/games.py
+++ games/games.py
@@ -6,6 +6,29 @@ title = "game"
 
 def rock_paper_scissors():
     easygui.msgbox('Здесь будет игра "Камень, ножницы, бумага"')
+    images = ['rock.png', 'scissors.png', 'paper.png']
+    a = {
+        0: 1,
+        1: 2,
+        2: 0
+    }
+
+    num = random.randrange(0, 3)
+    choice = easygui.buttonbox(
+        "Что выбирете?", "Камень, ножницы, бумага", images=images, choices=[])
+    if choice:
+        choice = images.index(choice)
+    else:
+        return None
+    if a[choice] == num:
+        easygui.buttonbox("Ты победил(а)!", "Камень, ножницы, бумага", images=[
+            images[choice], "gt.png", images[num]], choices=["Вернуться в меню"])
+    elif a[num] == choice:
+        easygui.buttonbox("Ты проиграл(а)!", "Камень, ножницы, бумага", images=[
+            images[choice], "lt.png", images[num]], choices=["Вернуться в меню"])
+    else:
+        easygui.buttonbox("Ничья!", "Камень, ножницы, бумага", images=[
+            images[choice], "eq.png", images[num]], choices=["Вернуться в меню"])
 
 
 num = random.randrange(1, 10)
@@ -21,6 +44,7 @@ def guess_the_number():
         easygui.msgbox("Нет число больше")
     else:
         easygui.msgbox("Угадал(а)")
+    easygui.msgbox(num)
 
 
 games = [
